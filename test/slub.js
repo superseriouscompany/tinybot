684b6f9179268cdf898d45ef085eaf4f91d068db
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-25 21:13:40 +0100

    Include express in dependencies

--- test/slub.js
+++ test/slub.js
@@ -3,43 +3,44 @@ var express         = require('express');
 var WebSocketServer = require('ws').Server;
 var app             = express();
 var debug           = require('debug')('tinybot:slub');
-app.get('*', function(req, res) {
-  res.json({
-    ok: true,
-    users: [
-      { name: 'neil', id: 'n0'},
-      { name: 'thebigdog', id: 's1'}
-    ],
-    channels: [
-      { name: 'general', id: 'CG0'}
-    ],
-    url: 'ws://localhost:6970'
-  })
-})
-
-var wss = new WebSocketServer({port: 6970});
-wss.on('connection', function(ws) {
-  module.exports.socket = {
-    send: function(message) {
-      if( message.text && !message.type ) { message.type = 'message'; }
-      ws.send(JSON.stringify(message), {mask: true});
-    },
-    shouldReceive: function(text, cb) {
-      ws.on('message', listener);
-
-      function listener(message) {
-        if( JSON.parse(message).text.match(text) ) {
-          cb();
-          ws.removeListener('message', listener);
-          debug(message, "Received expected message");
-          return;
-        }
-        debug(message, "unknown message");
-      }
-    }
-  }
-});
 
 module.exports.serve = function(port, cb) {
+  app.get('*', function(req, res) {
+    res.json({
+      ok: true,
+      users: [
+        { name: 'neil', id: 'n0'},
+        { name: 'thebigdog', id: 's1'}
+      ],
+      channels: [
+        { name: 'general', id: 'CG0'}
+      ],
+      url: 'ws://localhost:6970'
+    })
+  })
+
   app.listen(port || 6969, cb);
+
+  var wss = new WebSocketServer({port: 6970});
+  wss.on('connection', function(ws) {
+    module.exports.socket = {
+      send: function(message) {
+        if( message.text && !message.type ) { message.type = 'message'; }
+        ws.send(JSON.stringify(message), {mask: true});
+      },
+      shouldReceive: function(text, cb) {
+        ws.on('message', listener);
+
+        function listener(message) {
+          if( JSON.parse(message).text.match(text) ) {
+            cb();
+            ws.removeListener('message', listener);
+            debug(message, "Received expected message");
+            return;
+          }
+          debug(message, "unknown message");
+        }
+      }
+    }
+  });
 }
